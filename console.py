f5a9e763065a4f5a3d5835bc3e3643a3ba46b69c
zscbana <zscbana@gmail.com>
2024-03-10 02:40:03 +0200

    console.4

--- console.py
+++ console.py
@@ -4,15 +4,15 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """hbnb  command line interface."""
+    """ HBNH console """
     prompt = '(hbnb) '
+
+    def do_EOF(self, arg):
+        """Exits console"""
+        return True
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
-        return True
-
-    def do_EOF(self, arg):
-        """Exit the program when EOF is reached"""
         return True
 
 
