9de136d52036d8c3ca817183a62823a058f1cad6
IanGichengo <iangatimu6@gmail.com>
2024-03-10 08:09:17 -0400

    this includes the test folder and a test file

---
+++ tests/test_base_model.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import unittest
+from models.base_model import BaseModel
+import datetime
+
+
+class TestBaseModel(unittest.TestCase):
+    def setUp(self):
+        self.model = BaseModel()
+
+    def test_init(self):
+        self.assertIsInstance(self.model, BaseModel)
+        self.assertIsInstance(self.model.id, str)
+        self.assertIsInstance(self.model.created_at, datetime.datetime)
+        self.assertIsInstance(self.model.updated_at, datetime.datetime)
+
+    def test_to_dict(self):
+        model_dict = self.model.to_dict()
+        self.assertEqual(model_dict["__class__"], "BaseModel")
+        self.assertIsInstance(model_dict["created_at"], str)
+        self.assertIsInstance(model_dict["updated_at"], str)
+
+
+if __name__ == '__main__':
+    unittest.main()
