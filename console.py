6a0a1c342de49fa32fbe7ec4eba2fc87fb960dcc
zscbana <zscbana@gmail.com>
2024-03-10 02:42:44 +0200

    console.5

--- console.py
+++ console.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-""" console."""
+"""A simple command-line interpreter."""
+
 import cmd
 
-
 class HBNBCommand(cmd.Cmd):
-    """ HBNH console """
+    """HBNB console"""
     prompt = '(hbnb) '
 
     def do_EOF(self, arg):
-        """Exits console"""
+        """Exits console when Ctrl+D is pressed."""
         return True
 
     def do_quit(self, arg):
-        """Quit command to exit the program"""
+        """Quit command to exit the program."""
         return True
 
+    def emptyline(self):
+        """ overwriting the emptyline method """
+        return False
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
